5d76a0efce03f18ed8aca7a1d743be839980bf46
wahidari <wahiid.ari@gmail.com>
2022-01-15 15:28:07 +0700

    configure flowbite

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="assets/flowbite/flowbite.bundle.js" strategy='beforeInteractive' />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@themesberg/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // import in pages/_document.js
+        // use it in className "font-poppins" or "font-roboto"
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"]
+      },
+    },
   },
   plugins: [
     require('@themesberg/flowbite/plugin')
